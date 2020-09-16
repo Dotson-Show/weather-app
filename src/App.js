3b6244f6ad2fd84fe030fba1330b45f35c314270
Dotun Showunmi <showunmioludotun@gmail.com>
2020-09-16 08:41:46 +0100

    React Weather App Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { fetchWeather } from './api/fetchWeather';
+import './App.css';
+
 
 const App = () => {
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({}); 
+
+    const search = async (e) => {
+        if (e.key == 'Enter') {
+            const data = await fetchWeather(query);
+            setWeather(data);
+            setQuery('');
+            console.log(data);
+        }
+    }
     return (
-        <div>
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">Welcom to my App</h5>
-                    <p className="card-text">Content goes here</p>
+        <div className="main-container">
+            <input type="search" name="search" id="search" className="search" placeholder="Search . . ." value={query} 
+                onChange={(e) => setQuery(e.target.value)}onKeyPress={search}
+            />
+
+            {weather.main && (
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2>
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+                    <div className="info">
+                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <p>{weather.weather[0].description}</p>
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 };
