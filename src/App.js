8d02b67abcc6cac700c8d5feb81fe9c0e2fd5fbf
Dotun Showunmi <showunmioludotun@gmail.com>
2020-09-16 03:57:55 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Welcom to my App</h5>
+                    <p className="card-text">Content goes here</p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
